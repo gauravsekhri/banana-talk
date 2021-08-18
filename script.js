0fc5efc7cb4082c36ed7875182ebec3e8bdc4479
Gaurav Sekhri <gauravsekhri69@gmail.com>
2021-08-18 09:16:42 +0530

    new

--- script.js
+++ script.js
@@ -2,27 +2,25 @@ var translatebtn = document.querySelector('#transbtn');
 var textInput = document.querySelector('#text_area');
 var transOutput = document.querySelector('#output');
 
-InputString = textInput.value.replace(" ", "%20");
-
 // var transUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-var transUrl = "http://api.funtranslations.com/translate/gungan";
+var transUrl = "https://api.funtranslations.com/translate/minion.json";
 
+var reqUrl = transUrl + '?text=' + textInput.value;
 
-var reqUrl = transUrl + '?text=' + InputString;
-// var reqUrl = transUrl + '?text=' + 'i%20am%20gaurav';
-
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${transUrl}?text=${encodedURI}`;
+}
 
 function errorHandler(){
     alert("Error in connection !");
 }
 
 function consoleText(){
-    // console.log("hi", textInput.value);
-    // transOutput.innerText = textInput.value;
-    fetch(reqUrl).then(resp => resp.json()).then(newjson => (transOutput.innerText = newjson.contents.translated));
-    // fetch(reqUrl)
-    // .then(resp => resp.json())
-    // .then(newjson => console.log(newjson.contents.translated));
+    var text = textInput.value;
+    var finalURL = constructURL(text);
+    console.log(finalURL)
+    fetch(finalURL).then(resp => resp.json()).then(newjson => (transOutput.innerText = newjson.contents.translated)).catch(errorHandler);
 
 }
 
